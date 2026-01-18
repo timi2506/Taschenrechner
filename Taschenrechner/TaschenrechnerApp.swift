d85ce13282054a5f295cd56c0fbada569156af57
timi2506 <172171055+timi2506@users.noreply.github.com>
2026-01-18 12:18:26 +0100

    Initial Commit

---
+++ Taschenrechner/TaschenrechnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaschenrechnerApp.swift
+//  Taschenrechner
+//
+//  Created by Tim on 18.01.26.
+//
+
+import SwiftUI
+
+@main
+struct TaschenrechnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
